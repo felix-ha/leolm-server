849ebcb9391be3b75c4c9e258b754837d2629e46
felix-ha <hauerf98@gmail.com>
2023-10-28 23:22:53 +0200

    Fix chroma sqlite conflict

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
