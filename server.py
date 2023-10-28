1281bcd9ddb4d37dbdac2628f19a32ef1387c80f
felix-ha <hauerf98@gmail.com>
2023-10-28 23:01:13 +0200

    Cleanup code

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-#from transformers import pipeline
-#import torch
+from transformers import pipeline
+import torch
 
 from flask import Flask, request, jsonify
 import time
@@ -38,36 +38,6 @@ def mock_model(question, context):
 def server_is_online():
     logger.info("checked server status")
     return "online"
-
-# @app.route(route_model, methods=['POST'])
-# def llm():
-#     data = request.get_json()
-#     # context = data.get('context', None)
-
-#     question = data.get('question', "")
-#     prompt = data.get('prompt', None)
-
-#     if prompt:
-#         logger.info("prompt history received, continuing conversation")
-#         prompt = prompt + "<|im_end|>\n" + promt_new_question
-#     else:
-#         logger.info("no prompt history received, starting new conversation")
-#         prompt = promt_new_question
-
-#     # if context:
-#     #     logger.info("received context: " + context)
-
-#     logger.info("received question: " + question)
-
-
-#     start_time = time.perf_counter()
-#     result = generator(prompt.format(question=question), do_sample=True, top_p=0.95, max_length=8192) 
-#     end_time = time.perf_counter()
-
-#     response = {'answer': result, 'inference_time_seconds': end_time - start_time}
-#     logger.info("response length: " + len(str(response['answer'][0]['generated_text'])))
-#     logger.info("response: " + str(response))
-#     return jsonify(response)
 
 
 @app.route(route_model, methods=['POST'])
@@ -121,12 +91,12 @@ def upload():
             logging.info(f'context from file {context_from_file}')
 
         start_time = time.perf_counter()
-        # result = generator(prompt.format(question=question), do_sample=True, top_p=0.95, max_length=8192) 
+        result = generator(prompt.format(question=question), do_sample=True, top_p=0.95, max_length=8192) 
         result = "result"
         end_time = time.perf_counter()
 
         response = {'answer': result, 'inference_time_seconds': end_time - start_time}
-        # logger.info("response length: " + str(len(str(response['answer'][0]['generated_text']))))
+        logger.info("response length: " + str(len(str(response['answer'][0]['generated_text']))))
         logger.info("response: " + str(response))
         return jsonify(response)
     
@@ -139,7 +109,7 @@ if __name__ == '__main__':
     logger.info("starting server")
     logging.info("loading model")
     start_time = time.perf_counter()
-    #generator = pipeline(model="LeoLM/leo-mistral-hessianai-7b-chat", device="cuda", torch_dtype=torch.float16)
+    generator = pipeline(model="LeoLM/leo-mistral-hessianai-7b-chat", device="cuda", torch_dtype=torch.float16)
     end_time = time.perf_counter()
     logger.info("loaded model in " + str(end_time - start_time) + " seconds")
     app.run(host="0.0.0.0", port=port)
