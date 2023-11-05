ec2fc8532f379702627d4371377ce83925a552ba
felix-ha <hauerf98@gmail.com>
2023-11-05 16:13:35 +0100

    Use float16

--- models/blip2.py
+++ models/blip2.py
@@ -1,16 +1,17 @@
 import requests
 from PIL import Image
 from transformers import Blip2Processor, Blip2ForConditionalGeneration
+import torch
 
 def run():
     processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
-    model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", device="cuda")
+    model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16, device_map="auto")
 
     img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
     raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
 
     question = "how many dogs are in the picture?"
-    inputs = processor(raw_image, question, return_tensors="pt").to("cuda")
+    inputs = processor(raw_image, question, return_tensors="pt").to("cuda", torch.float16)
 
     out = model.generate(**inputs)
 
