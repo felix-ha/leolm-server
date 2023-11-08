ecbf63c47060af03585b7edb23bf7b5bbc7dc265
felix-ha <hauerf98@gmail.com>
2023-11-08 21:49:01 +0100

    Load model in global scope

--- server.py
+++ server.py
@@ -14,7 +14,29 @@ file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
+deploy_llm = os.getenv('DEPLOY_LLM', None)
+deploy_llm = None
+logger.info(f"DEPLOY_LLM: {deploy_llm}")
+try:
+    logger.info("starting server")
+    if deploy_llm:
+        logging.info("loading model")
+        start_time = time.perf_counter()
+        from transformers import pipeline
+        import torch  
+        generator = pipeline(model="LeoLM/leo-mistral-hessianai-7b-chat", device="cuda", torch_dtype=torch.float16)
+        end_time = time.perf_counter()
+        logger.info("loaded model in " + str(end_time - start_time) + " seconds")
+    else: 
+        logging.info("using mock model")
+        generator = None
+except Exception as e:
+    logger.exception(str(e))
+    exit(1)
+
+  
 app = FastAPI()
+
 
 port = 5000
 route_check = '/up-status'
@@ -33,7 +55,7 @@ def upload(question: str = Form(...), prompt: str = Form(None)):
     logger.info(f'received new chat message')
     try:
         logger.info(f'question: {question}')
-        response = ask_question(question, prompt)
+        response = ask_question(question, prompt, generator)
         logger.info(response)
         return response, 200
     
@@ -43,24 +65,5 @@ def upload(question: str = Form(...), prompt: str = Form(None)):
 
 
 if __name__ == '__main__':
-    deploy_llm = os.getenv('DEPLOY_LLM', None)
-    deploy_llm = None
-    logger.info(f"DEPLOY_LLM: {deploy_llm}")
-    try:
-        logger.info("starting server")
-        if deploy_llm:
-            logging.info("loading model")
-            start_time = time.perf_counter()
-            from transformers import pipeline
-            import torch  
-            generator = pipeline(model="LeoLM/leo-mistral-hessianai-7b-chat", device="cuda", torch_dtype=torch.float16)
-            end_time = time.perf_counter()
-            logger.info("loaded model in " + str(end_time - start_time) + " seconds")
-        else: 
-            logging.info("using mock model")
-            generator = None
-    except Exception as e:
-        logger.exception(str(e))
-        exit(1)
     uvicorn.run(app, host="0.0.0.0", port=port)
     logger.info("server stopped")
